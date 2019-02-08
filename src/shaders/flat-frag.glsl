2cfe115d0b0c39ca44ede3a7693797d7106602b2
Amy Chen <7achen@gmail.com>
2019-02-08 13:50:55 -0500

    anim cup

--- src/shaders/flat-frag.glsl
+++ src/shaders/flat-frag.glsl
@@ -9,6 +9,7 @@ in vec2 fs_Pos;
 out vec4 out_Col;
 
 vec3 light;
+vec3 nor;
 
 vec3 rayCast(vec4 s) {
     // multiply by far clip plane
@@ -18,7 +19,7 @@ vec3 rayCast(vec4 s) {
 
     // multiply by inverse projection matrix
     mat4 proj;
-    float fov = 90.0;
+    float fov = 45.0;
     float fov_rad = (fov * 3.14159) / 180.0;
     float S_f = tan(fov_rad / 2.0);
     float a = 1.0 / ((u_Dimensions.x / u_Dimensions.y) * S_f);
@@ -57,11 +58,11 @@ vec3 rayCast(vec4 s) {
     view = orient * transl;
     s = inverse(view) * s;
 
-    //set up ray
+    // set up ray
     vec3 origin = u_Eye;
     vec3 dir = normalize(vec3(s) - u_Eye);
 
-    //set light vector for lambert shading
+    // set light vector for shading
     light = vec3((inverse(view) * vec4(0.0, 0.0, 0.0, 1.0)) - vec4(fs_Pos, 1.0, 1.0));
 
     return dir;
@@ -95,7 +96,7 @@ float sect_op(float d1, float d2) {
 }
 
 mat4 rot_matrix(vec3 r) {
-  //convert angle to radians
+  // convert angle to radians
   r = (r * 3.14159) / 180.0;
 
   mat4 m;
@@ -120,8 +121,6 @@ mat4 rot_matrix(vec3 r) {
   rot_z[0][1] = sin(r.z);
 
   return rot_x * rot_y * rot_z;
-  //return rot_x;
-  //return m;
 }
 
 vec3 trans_pt(vec3 p, vec3 t) {
@@ -134,66 +133,119 @@ vec3 rot_op(vec3 r, vec3 p) {
   return p_rot;
 }
 
-float sceneSDF(vec3 p) {
-  //apply rotation + translation to box
-  vec3 rot_box = vec3(0.0, 0.0, 60.0);
+float cupSDF(vec3 p) {
+  vec3 anim_angle = vec3(0.0, 0.0, 0.0);
+  vec3 anim_trans = vec3(0.0, 0.5 * (sin(u_Time * 3.14159 * 0.01)), 0.0);
+  // apply rotation + translation to box point
+  vec3 rot_box = vec3(0.0, 45.0, 60.0);
+  rot_box += anim_angle;
   vec3 p_box = rot_op(rot_box, p);
-  vec3 t_box = vec3(2.3, 0.0, 0.0);
+  vec3 t_box = vec3(0.5, 0.0, 0.0);
+  //t_box += anim_trans;
 
-  //apply scale + translation to sphere
+  // apply scale + translation to sphere
   vec3 t_sph = vec3(0.0, 0.5, 0.0);
-  float dist_sph = sdf_sphere(trans_pt(p, t_sph) * 1.0, 3.0) / 1.0;
+  t_sph += anim_trans;
+  float dist_sph = sdf_sphere(trans_pt(p, t_sph) * 2.0, 3.0) / 2.0;
 
-  float dist_box = sdf_box(trans_pt(p_box, t_box) * 0.9, vec3(3.0, 3.0, 3.0)) / 0.9;
+  // create cup with sphere + box
+  //vec3 p_sph_box = mod(p_box, vec3(1.0, 1.0, 1.0)) - 0.5 * vec3(1.0, 1.0, 1.0);
+  float dist_box = sdf_box(trans_pt(p_box, t_box) * 1.4, vec3(1.2, 3.0, 3.0)) / 1.4;
   float dist_cup = sect_op(dist_box, dist_sph);
 
-  float dist_sph2 = sdf_sphere(p * 1.3, 5.0) / 1.3;
-  vec3 t_box2 = vec3(0.0, 4.0, 0.0);
-  float dist_box2 = sdf_box(trans_pt(p, t_box2) * 0.6, vec3(3.0, 3.0, 3.0)) / 0.6;
-  float dist_cup2 = sect_op(dist_box2, dist_sph2);
-
-  //cup handle
-  vec3 rot_torus = vec3(90.0, 0.0, 0.0);
+  // create cup handle with torus
+  vec3 rot_torus = vec3(65.0, 0.0, 0.0);
+  rot_torus += anim_angle;
   vec3 p_torus = rot_op(rot_torus, p);
-  vec3 t_tor = vec3(2.5, 0.0, 0.8);
-  float dist_torus = sdf_torus(trans_pt(p_torus, t_tor), vec2(1.2, 0.2));
+  vec3 t_tor = vec3(1.5, 0.0, -0.25);
+  t_tor += anim_trans;
+  float dist_torus = sdf_torus(trans_pt(p_torus, t_tor) * 1.6, vec2(1.2, 0.2)) / 1.6;
+
+  // inside of cup
+  vec3 t_sph2 = vec3(0.0, 0.5, 0.0);
+  t_sph2 += anim_trans;
+  float dist_sph2 = sdf_sphere(trans_pt(p, t_sph2) * 2.0, 2.7) / 2.0;
+  vec3 t_box2 = vec3(2.1, 0.0, 0.0);
+  //t_box2 += anim_trans;
+  float dist_box2 = sdf_box(trans_pt(p_box, t_box2) * 1.6, vec3(1.5, 3.0, 3.0)) / 1.6;
 
   dist_cup = sect_op(dist_box, dist_sph);
   float dist = union_op(dist_cup, dist_torus);
+  // cut out sphere from cup
+  float dist_inside = sect_op(dist_sph2, dist_box2);
+  // flat bottom of cup
+  dist = sub_op(dist_sph2, dist);
+  dist = union_op(dist_inside, dist);
+  return dist;
+}
+
+float coffeeSDF(vec3 p) {
+  vec3 anim_trans = vec3(0.0, 0.5 * (sin(u_Time * 3.14159 * 0.01)), 0.0);
+  // sphere
+  vec3 t_sph2 = vec3(0.0, 0.5, 0.0);
+  t_sph2 += anim_trans;
+  float dist_sph2 = sdf_sphere(trans_pt(p, t_sph2) * 2.0, 2.7) / 2.0;
+  // box
+  vec3 t_box3 = vec3(0.0, 2.8, 0.0);
+  t_box3 += anim_trans;
+  float dist_box3 = sdf_box(trans_pt(p, t_box3) * 1.6, vec3(3.0, 3.0, 3.0)) / 1.6;
+  float dist = sect_op(dist_sph2, dist_box3);
   return dist;
 }
 
 vec3 estNormal(vec3 p) {
   float eps = 0.001;
-  vec3 nor = vec3(sceneSDF(vec3(p.x + eps, p.y, p.z)) - sceneSDF(vec3(p.x - eps, p.y, p.z)),
-                  sceneSDF(vec3(p.x, p.y + eps, p.z)) - sceneSDF(vec3(p.x, p.y - eps, p.z)),
-                  sceneSDF(vec3(p.x, p.y, p.z + eps)) - sceneSDF(vec3(p.x, p.y, p.z - eps)));
+  vec3 nor = vec3(cupSDF(vec3(p.x + eps, p.y, p.z)) - cupSDF(vec3(p.x - eps, p.y, p.z)),
+                  cupSDF(vec3(p.x, p.y + eps, p.z)) - cupSDF(vec3(p.x, p.y - eps, p.z)),
+                  cupSDF(vec3(p.x, p.y, p.z + eps)) - cupSDF(vec3(p.x, p.y, p.z - eps)));
   return normalize(nor);
 }
 
-vec4 rayMarch(vec3 eye, vec3 dir) { 
-  //rayMarch returns (t,vec3 nor)
+vec2 rayMarch(vec3 eye, vec3 dir) { 
+  //rayMarch returns (t, object id)
   float t = 0.01;
-  int max_steps = 50;
+  int max_steps = 1000;
   for (int i = 0; i < max_steps; i++) {
     vec3 p = eye + t * dir;
 
-    float dist = sceneSDF(p);
-    vec3 nor = estNormal(p);
+    float dist = cupSDF(p);
+    float dist2 = coffeeSDF(p);
+    nor = estNormal(p);
 
-    if (dist < 0.001) {
-      //at surface
-      return vec4(t, nor);
+    if (dist < 0.00001) {
+      //at cup surface
+      return vec2(t, 1.0);
+      //move along ray
+      t += dist;
     }
-    //move along ray
-    t += dist;
+    else if (dist2 < 0.00001) {
+      //at coffee surface
+      return vec2(t, 2.0);
+      //move along ray
+      t += dist2;
+    }
+    else {
+      t += dist;
+    }
+  
     if (t >= 1000.0) {
       //end
-      return vec4(t, 0.0, 0.0, 0.0);
+      return vec2(t, 0.0);
     }
   }
   t = 1000.0;
-  return vec4(t, 0.0, 0.0, 0.0);
+  return vec2(t, 0.0);
+}
+
+float shadow(vec3 origin, vec3 dir, float mint, float maxt) {
+  for(float t = mint; t < maxt;) {
+    float h = rayMarch(u_Eye, dir)[0];
+    if(h < 0.001) {
+      return 0.0;
+    }
+    t += h;
+  }
+  return 1.0;
 }
 
 void main() {
@@ -203,28 +255,53 @@ void main() {
                 -1.0 * (1.0 - ((gl_FragCoord.y / u_Dimensions.y) * 2.0)), 1.0, 1.0);
   vec3 dir = rayCast(s);
 
-  vec4 march = rayMarch(u_Eye, dir);
+  float shadow_test = shadow(u_Eye, light, 0.01, 20.0);
+
+  vec2 march = rayMarch(u_Eye, dir);
+
+  //float shadow_test = shadow(u_Eye, light, 0.01, 20.0);
+  //vec4 shadow_col = vec4(vec3(shadow_test, shadow_test, shadow_test), 1.0);
+
   if (march[0] < 1000.0) {
-    //hit object
-    out_Col = vec4(0.5 * (dir + vec3(1.0, 1.0, 1.0)), 1.0);
+    // hit object
+    // out_Col = vec4(0.5 * (dir + vec3(1.0, 1.0, 1.0)), 1.0);
     
-    vec4 diffuseColor = vec4(0.5 * (dir + vec3(1.0, 1.0, 1.0)), 1.0);
+    if (march[1] == 1.0) {
+    // Cup ID
+    //vec4 diffuseColor = vec4(0.5 * (dir + vec3(1.0, 1.0, 1.0)), 1.0);
+    vec4 diffuseColor = vec4(vec3(0.65, 0.65, 1.0), 1.0);
+    //vec4 diffuseColor = vec4(vec3(0.65, 0.65 + (0.05 * sin(u_Time * 3.14159 * 0.01)), 1.0), 1.0);
     
-    // Calculate the diffuse term for Lambert shading
-    vec3 nor = vec3(march[1],march[2],march[3]);
-    
+    // Calculate the diffuse term for shading
     float diffuseTerm = dot(normalize(nor), normalize(light));
     // Avoid negative lighting values
     diffuseTerm = clamp(diffuseTerm, 0.0, 1.0);
+    diffuseTerm *= shadow_test;
     float ambientTerm = 0.2;
     float lightIntensity = diffuseTerm + ambientTerm;
+
+    // Implement specular light
+    vec4 H;
+    for (int i = 0; i < 4; i++) {
+        H[i] = (light[i] + u_Eye[i]) / 2.0;
+    }
+    float specularIntensity = max(pow(dot(normalize(H), normalize(vec4(nor,1.0))), 1.5), 0.0);
+
     // Compute final shaded color
-    out_Col = vec4(diffuseColor.rgb * lightIntensity, 1.0);
+    out_Col = vec4(diffuseColor.rgb * (lightIntensity + specularIntensity), 1.0);
+    }
+    else if (march[1] == 2.0) {
+      // Coffee ID; change color
+      out_Col = vec4(vec3(0.3, 0.1, 0.2), 1.0);
+    }
+  }
+  else {
+    // No valid ID; no intersection / set bg color
+    //out_Col = vec4(vec3(0.2, 0.2, 0.2), 1.0);
+    //out_Col = vec4(0.5 * (fs_Pos + vec2(1.0)), 0.5 * (sin(u_Time * 3.14159 * 0.01) + 1.0), 1.0);
+    out_Col = vec4(0.5 * (dir + vec3(1.0, 1.0, 1.0)), 1.0);
   }
 
-
   //out_Col = vec4(0.5 * (dir + vec3(1.0, 1.0, 1.0)), 1.0);
-  
   //out_Col = vec4(0.5 * (fs_Pos + vec2(1.0)), 0.5 * (sin(u_Time * 3.14159 * 0.01) + 1.0), 1.0);
-
 }
